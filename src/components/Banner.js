2967190eb82e64900deaaadf4eefa073e86d90ca
M. Hanif Azhary <hanif.hz@gmail.com>
2020-10-10 17:58:39 +0700

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,13 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>InspiYouth</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>InspiYouth merupakan komunitas kepemudaan<br />
+                yang bergerak di bidang pengembangan diri, edukasi, dan sosial.</p>
                 <ul className="actions">
-                    <li><a href="#program" className="button next scrolly">Get Started</a></li>
+                    <li><a href="#program" className="button next scrolly">Ketahui Lebih Lanjut</a></li>
                 </ul>
             </div>
         </div>
