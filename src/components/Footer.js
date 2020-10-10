d2b5d96a9efc27a06fc1258d5d1ae347871b17b3
M. Hanif Azhary <hanif.hz@gmail.com>
2020-10-10 01:32:18 +0000

    Update contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,15 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                <li><a href="https://www.facebook.com/inspiyouth.id/" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                <li><a href="https://www.instagram.com/inspiyouth.id/" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="https://www.youtube.com/channel/UCH8EQBAe2uA5ObgA3_lCcsg" className="icon alt fa-youtube-play"><span className="label">Youtube</span></a></li>
+                <li><a href="https://github.com/inspiyouth/" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                <li><a href="https://www.linkedin.com/company/inspiyouth/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
             </ul>
             <ul className="copyright">
-                <li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+                <li>&copy; InspiYouth</li>
+                <li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
             </ul>
         </div>
     </footer>
